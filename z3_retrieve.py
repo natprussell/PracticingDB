41afa88e495f400d7958e54da2a748453fba52ca
Natalie Russell <natprussell@gmail.com>
2024-03-06 09:25:52 -0600

    edited to pull columns from table

--- z3_retrieve.py
+++ z3_retrieve.py
@@ -6,7 +6,14 @@ cr=conn.cursor()
 #pulls list of all tables in listed database
 cr.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables=cr.fetchall()
-print(tables)
+print("list of tables in the DB file", tables,"\n")
+
+#found out table name is movies
+#this will pull all the columns from that table
+cr.execute('PRAGMA table_info({})'.format('movies'))
+columns=cr.fetchall()
+column_Names=[column[1] for column in columns]
+print('list of columns in the movie table\n.',column_Names,'\n')
 
 cr.execute("SELECT * FROM movies")
 print(cr.fetchall())
