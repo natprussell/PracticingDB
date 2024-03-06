1b83cdce33e7609e151ab53d87ae7303e082a449
Natalie Russell <natprussell@gmail.com>
2024-03-06 09:47:47 -0600

    added notes and template for adding movies at a later time

--- z2_populateTable.py
+++ z2_populateTable.py
@@ -10,9 +10,6 @@ def add_movie(title, director, year, genre):
     conn.commit()
     print("Movie Added :)")
 
-add_movie('Inception', 'Christopher Nolan',2010,'Sci-Fi')
-add_movie('The Shawshank Redemption', 'Frank Darabont', 1994, 'Drama')
-add_movie('The Godfather', 'Francis Ford Coppola', 1972, 'Crime, Drama')
-add_movie('Pulp Fiction', 'Quentin Tarantino', 1994, 'Crime, Drama')
-add_movie('The Dark Knight', 'Christopher Nolan', 2008, 'Action, Crime, Drama')
-add_movie('Forrest Gump', 'Robert Zemeckis', 1994, 'Drama, Romance')
+#template for adding new movies
+#add_movie('Inception', 'Christopher Nolan',2010,'Sci-Fi')
+#do not run the movie above... already added to database
