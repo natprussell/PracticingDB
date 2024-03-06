1212644774db32dd9bdaae9916ee5bcd5ffcd02d
Natalie Russell <natprussell@gmail.com>
2024-03-06 09:29:55 -0600

    add additional movies to database

--- z2_populateTable.py
+++ z2_populateTable.py
@@ -11,3 +11,8 @@ def add_movie(title, director, year, genre):
     print("Movie Added :)")
 
 add_movie('Inception', 'Christopher Nolan',2010,'Sci-Fi')
+add_movie('The Shawshank Redemption', 'Frank Darabont', 1994, 'Drama')
+add_movie('The Godfather', 'Francis Ford Coppola', 1972, 'Crime, Drama')
+add_movie('Pulp Fiction', 'Quentin Tarantino', 1994, 'Crime, Drama')
+add_movie('The Dark Knight', 'Christopher Nolan', 2008, 'Action, Crime, Drama')
+add_movie('Forrest Gump', 'Robert Zemeckis', 1994, 'Drama, Romance')
