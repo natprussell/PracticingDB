b85d0be89763104e28246227247d7a57e4a68fc6
Natalie Russell <natprussell@gmail.com>
2024-03-06 09:13:20 -0600

    update z3_retrieve file to call table names

--- z3_retrieve.py
+++ z3_retrieve.py
@@ -2,5 +2,11 @@ import sqlite3
 conn= sqlite3.connect('databaseFile.db')
 cr=conn.cursor()
 
+#uses database file specified with cr
+#pulls list of all tables in listed database
+cr.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables=cr.fetchall()
+print(tables)
+
 cr.execute("SELECT * FROM movies")
 print(cr.fetchall())
